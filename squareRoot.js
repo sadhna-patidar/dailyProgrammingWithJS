b4cf0eee4caf3a2e3fdf4b498495eb797404b601
Sadhna <sadhnapatidar804@gmail.com>
2023-11-08 17:08:09 +0530

    The squareRoot.js file added.

---
+++ squareRoot.js
@@ -0,0 +1,13 @@
+//Program to Find the Square Root of given Number
+
+const number1 = 120;
+const number2 = -80;
+const number3 = 'hello';
+
+const result1 = Math.sqrt(number1);
+const result2 = Math.sqrt(number2);
+const result3 = Math.sqrt(number3);
+
+console.log(`The square root of ${number1} is ${result1}`);
+console.log(`The square root of ${number2} is ${result2}`);
+console.log(`The square root of ${number3} is ${result3}`);
