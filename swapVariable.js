4541d6f808daf303b26ae313cf96978ede15a0d7
Sadhna <sadhnapatidar804@gmail.com>
2023-11-09 14:55:16 +0530

    The swapVariable.js file added.

---
+++ swapVariable.js
@@ -0,0 +1,11 @@
+//Program to Swap Two Variables values
+
+let x = 15;
+let y = 45;
+console.log("x=",x,"y=",y)
+let temp;
+temp = x;
+x = y;
+y = temp;
+console.log("After swapping:")
+console.log("x=",x,"y=",y)
