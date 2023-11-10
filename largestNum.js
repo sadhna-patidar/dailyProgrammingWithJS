c3eb04826d874dbe97371fb44234efea18dab5b6
Sadhna <sadhnapatidar804@gmail.com>
2023-11-10 17:16:27 +0530

    The largestNum.js file added.

---
+++ largestNum.js
@@ -0,0 +1,16 @@
+//Find the largest number among three numbers
+
+let num1= 43;
+let num2 = 70;
+let num3= 56;
+
+if(num1>num2 && num1>num3 ){
+    console.log ("the largest number is", num1 )
+}
+ else if(num2>num1 && num2>num3){
+    console.log ("the largest number is", num2 )
+}
+else{
+    console.log ("the largest number is", num3 )
+}
+
