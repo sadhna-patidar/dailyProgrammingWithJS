6c740565d3ef47a98494a3c67bfdd884fd40fbf2
Sadhna <sadhnapatidar804@gmail.com>
2023-11-20 14:46:10 +0530

    The posiNegZero.js file added.

---
+++ posiNegZero.js
@@ -0,0 +1,13 @@
+//Program to Check if a number is Positive, Negative, or Zero
+
+const number = -40;
+ 
+if (number > 0) { 
+	console.log("The number is positive"); 
+} 
+else if (number < 0) { 
+	console.log("The number is negative"); 
+} 
+else { 
+	console.log("The number is zero"); 
+};
