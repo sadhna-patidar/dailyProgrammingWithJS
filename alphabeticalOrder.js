3aae1060c2d5a463b694d2e0a406e21d2149dd90
Sadhna <sadhnapatidar804@gmail.com>
2023-12-28 19:58:39 +0530

    new js file added.

---
+++ alphabeticalOrder.js
@@ -0,0 +1,19 @@
+function sortWordsAlphabetically(sentence) {
+    // Split the sentence into an array of words
+    const words = sentence.split(/\s+/);
+
+    // Sort the array of words in alphabetical order
+    const sortedWords = words.sort((a, b) => a.localeCompare(b, 'en', { sensitivity: 'base' }));
+
+    // Join the sorted words back into a sentence
+    const sortedSentence = sortedWords.join(' ');
+
+    return sortedSentence;
+}
+
+// Example usage
+const inputSentence = "This is an example sentence for sorting words.";
+const sortedSentence = sortWordsAlphabetically(inputSentence);
+
+console.log("Original sentence:", inputSentence);
+console.log("Sorted sentence:", sortedSentence);
