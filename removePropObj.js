351c6ea5bf9a4d4d5aa8822a84ec691c74a756fb
Sadhna <sadhnapatidar804@gmail.com>
2024-01-05 17:33:53 +0530

    The removePropObj.js file added.

---
+++ removePropObj.js
@@ -0,0 +1,12 @@
+//Program to Remove a Property from an Object
+
+const person = {
+    name: 'John',
+    age: 30,
+    gender: 'male'
+  };
+  
+  
+  delete person.age;
+  console.log(person);
+  
