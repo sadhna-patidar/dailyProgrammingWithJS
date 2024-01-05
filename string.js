7906fd98aeca75b968ab54c25ec39221cd53ad70
Sadhna <sadhnapatidar804@gmail.com>
2024-01-05 17:38:22 +0530

    The string.js file added.

---
+++ string.js
@@ -0,0 +1,13 @@
+//Check Whether a String Starts and Ends With Certain Characters
+
+const myString = "Hello, World!";
+
+// Check if the string starts with "Hello"
+const startsWithHello = myString.startsWith("Hello");
+
+// Check if the string ends with "World!"
+const endsWithWorld = myString.endsWith("World!");
+
+// Display the results
+console.log(`String starts with "Hello": ${startsWithHello}`);
+console.log(`String ends with "World!": ${endsWithWorld}`);
