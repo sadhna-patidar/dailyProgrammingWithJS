53699f36ecf5118d92be5e338d3312ac38706f0a
Sadhna <sadhnapatidar804@gmail.com>
2024-01-09 17:17:09 +0530

    The objLoop.js file added.

---
+++ objLoop.js
@@ -0,0 +1,38 @@
+
+//for...in loop:
+const myObject = {
+    name: 'Sadhna',
+    age : '21'
+  };
+  
+  for (const key in myObject) {
+    if (myObject.hasOwnProperty(key)) {
+      const value = myObject[key];
+      console.log(`${key}: ${value}`);
+    }
+  }
+ 
+  //Object.keys() method:
+  const myObject1 = {
+    username: 'sadhna',
+    email : 'sadhna@123.com',
+    password: 'sadhna123'
+  };
+  
+  Object.keys(myObject1).forEach(key => {
+    const value = myObject1[key];
+    console.log(`${key}: ${value}`);
+  });
+
+  //Object.entries() method
+  const myObject2 = {
+    key1: 'value1',
+    key2: 'value2',
+    key3: 'value3'
+  };
+  
+  Object.entries(myObject2).forEach(([key, value]) => {
+    console.log(`${key}: ${value}`);
+  });
+  
+  
