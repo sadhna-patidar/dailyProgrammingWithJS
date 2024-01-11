ed7d56fd0f49ce0600530b36f78483e651901c87
Sadhna <sadhnapatidar804@gmail.com>
2024-01-11 17:40:37 +0530

    The addKeyValue.js file added.

---
+++ addKeyValue.js
@@ -0,0 +1,10 @@
+//JavaScript Program to Add Key/Value Pair to an Object
+let myObject = {
+    name: 'sadhna',
+    suranme: 'patidar'
+  };
+  
+  myObject.age = '21';
+  
+  console.log(myObject);
+  
