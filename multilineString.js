f55dcd348e889f80081e5e0500f832c68990c0c5
Sadhna <sadhnapatidar804@gmail.com>
2024-01-15 19:12:10 +0530

    The multilineString.js file added.

---
+++ multilineString.js
@@ -0,0 +1,11 @@
+//JavaScript Program to Create Multiline Strings
+const multilineString = `
+  This is a multiline string
+  spanning multiple lines.
+  
+  You can include variables like ${2 + 2}.
+  
+  Special characters like \n and \t work as expected.
+`;
+
+console.log(multilineString);
