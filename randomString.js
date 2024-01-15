b113ae8355b1a0391ff7ccce3250a14bd791e094
Sadhna <sadhnapatidar804@gmail.com>
2024-01-15 19:36:22 +0530

    The randomString.js file added.

---
+++ randomString.js
@@ -0,0 +1,16 @@
+//JavaScript Program to Generate Random String
+function generateRandomString(length) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let randomString = '';
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      randomString += characters.charAt(randomIndex);
+    }
+  
+    return randomString;
+  }
+  
+  // Example: Generate a random string of length 10
+  const randomString = generateRandomString(10);
+  console.log(randomString);
